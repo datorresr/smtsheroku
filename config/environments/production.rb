f408fe504b0c23aab11f8ff6611e8ad4362b0934
Diego Alexander Torres <31226887+datorresr@users.noreply.github.com>
2017-11-06 17:18:37 -0500

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,6 +13,7 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   #config.action_controller.perform_caching = true
+  require 'dalli'
   config.action_controller.perform_caching = true
   config.cache_store = :dalli_store,
                     (ENV["MEMCACHIER_SERVERS"] || "").split(","),
