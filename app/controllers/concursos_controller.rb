58d7a4dd4ad93f28d2f9d3ce7168ad6456efc2fd
datorresr <datorresr@gmail.com>
2017-11-06 03:30:54 -0500

    make it better

--- app/controllers/concursos_controller.rb
+++ app/controllers/concursos_controller.rb
@@ -4,8 +4,7 @@ class ConcursosController < ApplicationController
 
   def show
     @concurso = Concurso.find(params[:id])
-	@video = @concurso.videos.build 
-    #@video = Video.new
+    @video = Video.new
     @videos = Video.all
     @vfinals = Array.new
     @videos.each { |v| s = v.concurso_ids
